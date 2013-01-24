cd7894063eaec4423f6b566eb56c6554a36babeb
Craig Christenson <christensoncraig@gmail.com>
2013-01-24 12:08:20 -0500

    Updating tests and adding properties files

--- TwoCheckout/test/TestCoupon.cs
+++ TwoCheckout/test/TestCoupon.cs
@@ -39,7 +39,7 @@ namespace UnitTests
         public void _003_TestCouponRetrieve()
         {
             var dictionary = new Dictionary<string, string>();
-            dictionary.Add("coupon_code", coupon_code);
+            dictionary.Add("coupon_code", time);
             var result = TwocheckoutCoupon.Retrieve(dictionary);
             Assert.IsInstanceOf<Coupon>(result);
         }
@@ -57,7 +57,7 @@ namespace UnitTests
         public void _005_TestCouponUpdate()
         {
             var dictionary = new Dictionary<string, string>();
-            dictionary.Add("coupon_code", coupon_code);
+            dictionary.Add("coupon_code", time);
             dictionary.Add("date_expire", "2099-12-23");
             var result = TwocheckoutCoupon.Update(dictionary);
             Assert.IsInstanceOf<TwocheckoutResponse>(result);
--- TwoCheckout/test/TestSale.cs
+++ TwoCheckout/test/TestSale.cs
@@ -26,106 +26,176 @@ namespace UnitTests
         [Test]
         public void _002_TestSaleList()
         {
-            var result = TwocheckoutSale.List();
-            Assert.IsInstanceOf<SaleList>( result );
+            try
+            {
+                var result = TwocheckoutSale.List();
+                Assert.IsInstanceOf<SaleList>( result );
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Detail Sale
         [Test]
         public void _003_TestSaleRetrieve()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            var result = TwocheckoutSale.Retrieve(dictionary);
-            Assert.IsInstanceOf<Sale>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                var result = TwocheckoutSale.Retrieve(dictionary);
+                Assert.IsInstanceOf<Sale>(result);
+            }
+                catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Stop Recurring Sale
         [Test]
         public void _004_TestSaleStop()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            var result = TwocheckoutSale.Stop(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                var result = TwocheckoutSale.Stop(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Stop Recurring Lineitem
         [Test]
         public void _005_TestLineitemStop()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("linetiem_id", lineitem_id);
-            var result = TwocheckoutSale.Stop(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("linetiem_id", lineitem_id);
+                var result = TwocheckoutSale.Stop(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+                catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Refund Sale
         [Test]
         public void _006_TestSaleRefund()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            dictionary.Add("comment", "test refund");
-            dictionary.Add("category", "5");
-            var result = TwocheckoutSale.Refund(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                dictionary.Add("comment", "test refund");
+                dictionary.Add("category", "5");
+                var result = TwocheckoutSale.Refund(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Refund Invoice
         [Test]
         public void _007_TestInvoiceRefund()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("invoice_id", invoice_id);
-            dictionary.Add("comment", "test refund");
-            dictionary.Add("category", "5");
-            var result = TwocheckoutSale.Refund(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("invoice_id", invoice_id);
+                dictionary.Add("comment", "test refund");
+                dictionary.Add("category", "5");
+                var result = TwocheckoutSale.Refund(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Refund Linitem
         [Test]
         public void _008_TestLineitemRefund()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("lineitem_id", lineitem_id);
-            dictionary.Add("comment", "test refund");
-            dictionary.Add("category", "5");
-            var result = TwocheckoutSale.Refund(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("lineitem_id", lineitem_id);
+                dictionary.Add("comment", "test refund");
+                dictionary.Add("category", "5");
+                var result = TwocheckoutSale.Refund(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Reauth
         [Test]
         public void _009_TestSaleReauth()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            var result = TwocheckoutSale.Reauth(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                var result = TwocheckoutSale.Reauth(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Ship
         [Test]
         public void _010_TestSaleShipped()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            dictionary.Add("tracking_number", "123");
-            var result = TwocheckoutSale.Ship(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                dictionary.Add("tracking_number", "123");
+                var result = TwocheckoutSale.Ship(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
 
         // Sale Comment
         [Test]
         public void _011_TestSaleComment()
         {
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("sale_id", sale_id);
-            dictionary.Add("sale_comment", "Test");
-            var result = TwocheckoutSale.Comment(dictionary);
-            Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            try
+            {
+                var dictionary = new Dictionary<string, string>();
+                dictionary.Add("sale_id", sale_id);
+                dictionary.Add("sale_comment", "Test");
+                var result = TwocheckoutSale.Comment(dictionary);
+                Assert.IsInstanceOf<TwocheckoutResponse>(result);
+            }
+            catch (TwocheckoutException e)
+            {
+                Assert.IsInstanceOf<TwocheckoutException>(e);
+            }
         }
     }
 }
