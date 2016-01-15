c7fae0dc6e3ca0c22f09b8b653a222b6143aa23d
Craig Christenson <craigchristenson@fastmail.com>
2016-01-15 00:07:51 -0500

    Fix for recurring decline

---
+++ TwoCheckout/Entities/RecurringDecline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoCheckout
+{
+    public class RecurringDecline
+    {
+        public DateTime? date_declined { get; set; }
+        public DateTime? date_updated { get; set; }
+        public string decline_code { get; set; }
+        public string pay_method { get; set; }
+        public string pplus_code { get; set; }
+        public int retries { get; set; }
+        public long? sale_id { get; set; }
+    }
+}
--- TwoCheckout/Entities/Sale.cs
+++ TwoCheckout/Entities/Sale.cs
@@ -7,7 +7,7 @@ namespace TwoCheckout
         public DateTime date_placed { get; set; }
         public string ip_address { get; set; }
         public string ip_country { get; set; }
-        public string recurring_decline { get; set; }
+        public RecurringDecline recurring_decline { get; set; }
         public long? sale_id { get; set; }
         public Invoice[] invoices { get; set; }
         public Comments[] comments { get; set; }
